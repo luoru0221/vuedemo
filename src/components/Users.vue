116ddffc394c6558ebb630a87b1a446141574a40
luoru0221 <331980169@qq.com>
2020-02-20 22:00:12 +0800

    完成用户管理模块

--- src/components/Users.vue
+++ src/components/Users.vue
@@ -27,16 +27,32 @@
         <el-table-column label="邮箱" prop="email"></el-table-column>
         <el-table-column label="电话" prop="mobile"></el-table-column>
         <el-table-column label="角色" prop="role_name"></el-table-column>
-        <el-table-column label="状态" prop="mg_state">
-          <template>
-            <el-switch v-model="value">
+        <!--通过作用域插槽自定义数据展示方式-->
+        <el-table-column label="状态">
+          <template slot-scope="scope">
+            <el-switch v-model="scope.row.mg_state">
             </el-switch>
+          </template>
+        </el-table-column>
+
+        <el-table-column label="操作" width="190px">
+          <template slot-scope="scope">
+            <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <el-tooltip class="item" effect="dark" content="分配角色" placement="top" :enterable="false">
+              <el-button type="warning" icon="el-icon-setting" size="mini"></el-button>
+            </el-tooltip>
           </template>
         </el-table-column>
       </el-table>
 
       <!-- 分页-->
-      <el-pagination>
+      <el-pagination @size-change="handleSizeChange" @current-change="handleCurrentChange"
+        :current-page="page.pagenum"
+        :page-sizes="[1, 2, 5, 10]"
+        :page-size="page.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total">
       </el-pagination>
     </el-card>
 
@@ -49,9 +65,13 @@
         data() {
             return {
                 users: [],
-                query: '',
-                pagenum: 1,
-                pagesize: 5
+
+                page: {
+                    query: '',
+                    pagenum: 1,
+                    pagesize: 1
+                },
+                total:0
             }
         },
         created() {
@@ -61,11 +81,7 @@
             /*获取用户数据*/
             getUsers() {
                 this.axios.get('/users', {
-                    params: {
-                        query: this.query,
-                        pagenum: this.pagenum,
-                        pagesize: this.pagesize
-                    }
+                    params:this.page
                 }).then(response => {
                     let res = response.data;
                     console.log(res);
@@ -73,8 +89,19 @@
                         return this.$message.error("获取用户列表失败");
                     }
                     this.users = res.data.users;
+                    this.total = res.data.total;
                 })
-            }
+            },
+            //监听pageSize的改变
+            handleSizeChange(newSize){
+                this.page.pagesize = newSize;
+                this.getUsers();
+            },
+            //监听pageNum的改变
+            handleCurrentChange(newPageNum){
+                this.page.pagenum = newPageNum;
+                this.getUsers();
+            },
         }
     }
 </script>
